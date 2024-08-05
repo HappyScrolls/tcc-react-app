287dcfb3a21a628bcf893366179e1c60660a3d65
chs98412 <chs98412@gmail.com>
2024-08-05 16:38:52 +0900

    카카오로그인 추가 세팅 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, {useEffect} from 'react';
+import {BrowserRouter as Router, Route, Routes, useLocation} from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
+import KakaoLogin from "./pages/KakaoLogin";
+import axios from "axios/index";
 
+const OAuth2RedirectHandler = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        const params = new URLSearchParams(location.search);
+        const code = params.get('code');
+        // if (code) {
+        //     // Spring Boot의 /login/oauth2/code/kakao 경로로 리다이렉트
+        //     window.location.href = `http://localhost:8080/login/oauth2/code/kakao?code=${code}`;
+        // }
+
+
+
+    }, [location]);
+
+
+    return <div>Redirecting...</div>;
+};
 const App = () => {
     return (
         <Router>
@@ -11,10 +31,13 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
+                    <Route path="/login" element={<KakaoLogin/>} />
+                    <Route path="/login/oauth2/code/kakao" element={<OAuth2RedirectHandler/>} />
                 </Routes>
             </Layout>
         </Router>
     );
 };
+
 
 export default App;
