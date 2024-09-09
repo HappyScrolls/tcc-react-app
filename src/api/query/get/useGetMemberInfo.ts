10ada44981303e2ceec076b7c26d79724af46aa7
chs98412 <chs98412@gmail.com>
2024-09-09 23:27:42 +0900

    login

---
+++ src/api/query/get/useGetMemberInfo.ts
@@ -0,0 +1,25 @@
+import {axiosInstance, memberAxiosInstance} from "../../axios";
+import { ISchedule } from "../../../types/ISchedule";
+import {IMemberInfo} from "../../../types/IMemberInfo";
+
+interface useGetMemberInfoProps {
+    memberCode: string;
+}
+
+async function useGetMemberInfo({memberCode}: useGetMemberInfoProps): Promise<IMemberInfo | undefined> {
+
+    try {
+        const response = await memberAxiosInstance.get(`/member`, {
+            headers: {
+                "Member-Code": memberCode, // 헤더에 memberCode 추가
+            },
+        });
+
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching member info:", error);
+        throw error;
+    }
+}
+
+export default useGetMemberInfo;
---
+++ src/types/IMemberInfo.ts
@@ -0,0 +1,9 @@
+export interface IMemberInfo {
+    no: number;
+    accountId: string|null;
+    name: string|null;
+    email: string|null;
+    profilePhoto: string|null;
+    birthDate: string|null;
+    mobileNo: string|null;
+}
