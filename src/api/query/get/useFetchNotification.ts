a1b12d008112941cc3ebe1cf91945afc444478fa
mnbvcxzyj <mnbvcxzyj@gmail.com>
2024-09-24 22:08:59 +0900

    remove : 사용하지 않는 변수 삭제

--- src/api/query/get/useFetchNotification.ts
+++ src/api/query/get/useFetchNotification.ts
@@ -1,23 +1,22 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
-import {axiosInstance, notificationAxiosInstance} from "../../axios";
-import {INotification} from "../../../types/INotification";
-import {ISchedule} from "../../../types/ISchedule";
+import { notificationAxiosInstance } from "../../axios";
+import { INotification } from "../../../types/INotification";
 
+const useFetchNotification = (): INotification[] => {
+  const getFetchNotification = async (): Promise<INotification[]> => {
+    const response = await notificationAxiosInstance.get(
+      `/notification-service`
+    );
 
-const useFetchNotification = () : INotification[] => {
-    const getFetchNotification = async (): Promise<INotification[]> => {
-        const response = await notificationAxiosInstance.get(`/notification-service`);
+    return response.data;
+  };
 
-        return response.data;
-    };
+  const { data: notifications } = useSuspenseQuery({
+    queryKey: ["fetchNotification"],
+    queryFn: () => getFetchNotification(),
+  });
 
-    const { data: notifications } = useSuspenseQuery({
-        queryKey: ["fetchNotification"],
-        queryFn: ()=>getFetchNotification(),
-    });
-
-    return notifications;
+  return notifications;
 };
-
 
 export default useFetchNotification;
