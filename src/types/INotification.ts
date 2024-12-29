34c054e73e967e5ea238548c3f555457a68e4d36
chs98412 <chs98412@gmail.com>
2024-12-29 23:49:33 +0900

    enum 추가

--- src/types/INotification.ts
+++ src/types/INotification.ts
@@ -12,9 +12,11 @@ export enum NotificationType {
     SCHEDULE_CREATE = "SCHEDULE_CREATE",
     SCHEDULE_MODIFY_REQUEST = "SCHEDULE_MODIFY_REQUEST",
     SCHEDULE_MODIFY_REQUEST_ACCEPTED = "SCHEDULE_MODIFY_REQUEST_ACCEPTED",
+    SCHEDULE_MODIFY_REQUEST_REJECTED = "SCHEDULE_MODIFY_REQUEST_REJECTED",
 }
 export const NotificationTypeMessages: Record<NotificationType, string> = {
     [NotificationType.SCHEDULE_CREATE]: "일정 생성",
     [NotificationType.SCHEDULE_MODIFY_REQUEST]: "일정 수정 요청",
     [NotificationType.SCHEDULE_MODIFY_REQUEST_ACCEPTED]: "일정 수정 요청 수락",
+    [NotificationType.SCHEDULE_MODIFY_REQUEST_REJECTED]: "일정 수정 요청 거절",
 };
